7cf67f6c35e29576b9433af095936446f828e6f4
77lucaw <77lucawalker@gmail.com>
2025-09-23 20:35:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function sayHello() {
-  alert("Hello from QuickEdit!");
+// Show a popup when page loads
+alert("Welcome to my website!");
+
+// Function to change text
+function changeMessage() {
+  document.getElementById("message").innerText = "You clicked the button!";
 }
